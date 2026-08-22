59fbe96494ba916759ce1612af627ea7fab49ecf
Akshay <services.techxit@gmail.com>
2026-08-22 23:21:13 +0530

    Fix host binding for Hostinger

--- admin/backend/src/index.ts
+++ admin/backend/src/index.ts
@@ -64,6 +64,6 @@ app.use((err: any, req: express.Request, res: express.Response, next: express.Ne
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT as number, '0.0.0.0', () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
